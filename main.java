d6e1f59c44dfe8443563795020a520e742544ed6
23maxb <23maxb@students.harker.org>
2021-09-19 07:54:26 -0700

    mainedit
    maineditted

--- main.java
+++ main.java
@@ -3,5 +3,6 @@ import java.util.*;
 public class main {
     public static void main(String[] args) throws Exception {
         System.out.println((int) diceRoller.multiRoll("2d20"));
+        System.out.println((int) diceRoller.multiRoll("2d20"));
     }
 }
