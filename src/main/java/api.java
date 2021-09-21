e76c1c385c7fe6be8c1736986da6ed9926ac03fa
23maxb <23maxb@students.harker.org>
2021-09-21 15:32:29 -0700

    api begun
    api has begun support

---
+++ src/main/java/api.java
@@ -0,0 +1,16 @@
+import static spark.Spark.*;
+
+public class api {
+    public static String search(String s)
+    {
+        final String[] ret = new String[1];
+
+        get("https://www.dnd5eapi.co/api/" + s, (req, res) -> {
+            ret[0] =res.body();
+            return 200;
+        });
+        return ret[0];
+    }
+
+    
+}
