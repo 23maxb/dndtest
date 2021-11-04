4c49e3ddfbadeca3bf9ccebba6c47954b34b992b
23maxb <23maxb@students.harker.org>
2021-11-04 15:43:22 -0700

    api more updated
    api more updated

--- src/main/java/api.java
+++ src/main/java/api.java
@@ -11,8 +11,8 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 public class api {
-    public static @NotNull String searchcurl(String urltocurl) throws Exception {
-        URL url = new URL(urltocurl);
+    public static @NotNull String searchCurl(String urlToCurl) throws Exception {
+        URL url = new URL(urlToCurl);
         StringBuilder toReturn = new StringBuilder();
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream(), StandardCharsets.UTF_8))) {
             for (String line; (line = reader.readLine()) != null; ) toReturn.append("\n").append(line);
@@ -20,19 +20,27 @@ public class api {
         return toReturn.toString();
     }
 
+    @SuppressWarnings("unchecked")
     public static Map<String, String> jsonToMap(@NotNull String JSON_SOURCE) throws Exception {
         return (Map<String, String>) (new ObjectMapper().readValue(JSON_SOURCE, HashMap.class));
     }
 
-    public static Map<String, String> get5eEntryPath(String @NotNull ... args) throws Exception {
+
+    public static @NotNull String get5eEntryPath(String @NotNull ... args) throws Exception {
         String path = Arrays.stream(args).map(arg -> arg + "/").collect(Collectors.joining("", "/api/", ""));
         System.out.println("https://www.dnd5eapi.co" + path);
-        return jsonToMap(searchcurl("https://www.dnd5eapi.co" + path));
+        return searchCurl("https://www.dnd5eapi.co" + path);
     }
 
     public static String getValueOfItem(String @NotNull ... args) throws Exception {
         String[] a = new String[args.length - 1];
         System.arraycopy(args, 0, a, 0, a.length);
-        return get5eEntryPath(a).get(args[args.length - 1]);
+        return jsonToMap(get5eEntryPath(a)).get(args[args.length - 1]);
     }
+
+    public static Map<String, String> getValueOfPath(String @NotNull ... args) throws Exception {
+        return jsonToMap(get5eEntryPath(args));
+    }
+
+
 }
--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,10 +1,11 @@
-import java.util.Map;
-
-
 public class main {
     public static void main(String[] args) throws Exception {
-        System.out.println(api.searchcurl("https://www.dnd5eapi.co/api/spells/acid-arrow"));
-        Map<String, String> a = api.jsonToMap(api.searchcurl("https://www.dnd5eapi.co/api/spells/acid-arrow"));
+        /*
+        System.out.println(api.searchCurl("https://www.dnd5eapi.co/api/spells/acid-arrow"));
+        Map<String, String> a = api.jsonToMap(api.searchCurl("https://www.dnd5eapi.co/api/spells/acid-arrow"));*/
         System.out.println(api.getValueOfItem("monsters", "adult-black-dragon", "type"));
+        System.out.println(api.getValueOfPath("equipment", "spear"));
+        System.out.println(api.getValueOfItem("equipment", "spear", "damage"));
+
     }
 }
