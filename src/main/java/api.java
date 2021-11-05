a7267f27b2e53bb10ac32c68e06fc79d455f275d
23maxb <23maxb@students.harker.org>
2021-11-04 20:48:02 -0700

    API now completely done with proper accessing
    still need to add search function though

--- src/main/java/api.java
+++ src/main/java/api.java
@@ -10,6 +10,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+//Use api.getAtribute("catagory", "subcatagory", "subcatagory", "atribute");
 public class api {
     public static @NotNull String searchCurl(String urlToCurl) throws Exception {
         URL url = new URL(urlToCurl);
@@ -28,7 +29,6 @@ public class api {
 
     public static @NotNull String get5eEntryPath(String @NotNull ... args) throws Exception {
         String path = Arrays.stream(args).map(arg -> arg + "/").collect(Collectors.joining("", "/api/", ""));
-        System.out.println("https://www.dnd5eapi.co" + path);
         return searchCurl("https://www.dnd5eapi.co" + path);
     }
 
@@ -45,22 +45,23 @@ public class api {
     public static Object getAtribute(String @NotNull ... args) throws Exception {
         Object toReturn = getValueOfPath(args[0]);
         for (int i = 0; i < args.length; i++) {
+            assert toReturn != null : "The argument " + args[i - 1] + " was invalid and caused an error.";
             String[] a = new String[i + 1];
             System.arraycopy(args, 0, a, 0, a.length);
             try {
-                //Comment the following line out in testing
-                getValueOfPath(a);
                 toReturn = getValueOfPath(a);
             } catch (Exception E) {
-
                 assert (toReturn instanceof Map<?, ?>);
-                toReturn = ((Map<?, ?>) toReturn).get(args[i]);
-                if (toReturn instanceof String && i == args.length-1)
+                try {
+                    toReturn = ((Map<?, ?>) toReturn).get(args[i]);
+                } catch (Exception A) {
+                    throw new IllegalArgumentException("The argument " + args[i - 1] + " was invalid and caused an error.");
+                }
+                if (toReturn instanceof String && i == args.length - 1)
                     return toReturn;
             }
-
         }
-
+        return toReturn;
     }
 
 
--- src/main/java/main.java
+++ src/main/java/main.java
@@ -2,12 +2,7 @@ import java.util.Map;
 
 public class main {
     public static void main(String[] args) throws Exception {
-        /*
-        System.out.println(api.searchCurl("https://www.dnd5eapi.co/api/spells/acid-arrow"));
-        Map<String, String> a = api.jsonToMap(api.searchCurl("https://www.dnd5eapi.co/api/spells/acid-arrow"));*/
-        //System.out.println(api.getValueOfItem("monsters", "adult-black-dragon", "type"));
-        //System.out.println(api.getValueOfPath("equipment", "spear").get("damage"));
-        System.out.println(api.getAtribute( "equipment"));
+        System.out.println(api.getAtribute( "equipment", "spear", "damage", "damage_dice"));
 
     }
 }
